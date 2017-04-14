6bfa750d088d6ffae6d21a67a703ac42d0b441ec
root <root@ximena.(none)>
2017-04-14 17:42:24 +0800

    初始化用户资源

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,17 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+require 'rubygems'
+require 'rails/commands/server'
+
+=begin
+module Rails
+  class Server
+    alias :default_options_bk :default_options
+    def default_options
+      default_options_bk.merge!(Host: '120.27.94.221')
+    end
+  end
+end
+=end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  
+  resources :users
+ 
+  get 'main/index'
+
+  get 'login/index'
+
+  root 'main#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
