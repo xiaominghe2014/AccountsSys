1e61ecfe7dda80cb2f48a1d5a5d23ae2c2ab3455
xiaominghe2014@gmail.com <xiaominghe2014@gmail.com>
2017-04-18 15:58:39 +0800

    添加用户数据以及展示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,27 @@ class UsersController < ApplicationController
 	end
 
   def create
-    render plain:params[:user].inspect
+    # render plain:params[:user].inspect
+    # @user = User.new(params[:user])
+    # @user = User.new(params.require(:user).permit(:name,:pwd))
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.all
+  end
+
+  private
+    def user_params
+        params.require(:user).permit(:name, :pwd)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  # validates :name,  presence: true,
+  #                   length: {  minimum :  5  }
+  #validates :pwd,presence :true,length:{minimum :5}
 end
